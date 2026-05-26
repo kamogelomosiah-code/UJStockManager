7e82e196eeea75362a54d8ae3d9b2a194bd7f758
kamogelo Mosia <kamogelomosiah@gmail.com>
2026-05-26 15:02:25 +0000

    jj

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  const base = mode === 'production' ? '/UJStockManager/' : '/';
+
   return {
-    base: './',
+    base,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
